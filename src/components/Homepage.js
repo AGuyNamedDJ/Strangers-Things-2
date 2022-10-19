1c193d36de6c4cce7c9c7dc319faf5700e9c89ac
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-19 14:28:33 -0500

    Cleaning restarting point

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,10 @@ import React from "react";
 const Homepage = () => {
     return (
         <div>
-            <p>Bonjou! I am a Homepage!</p>
+            <h1>Bonjou! I am a Homepage!</h1>
+            <p>`I can't rememeber the code to put in random data.
+                It was something like Latin250 and it would print out 250 words.`
+            </p>
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from "react";
-import ReactDom from "react-dom";
-import { createBrowerRouter, RouterProvider } from "react-router-dom";
-import Homepage from "./components/Homepage";
-import ErrorPage from "./components/ErrorPage";
+import ReactDOM from "react-dom";
 
-const router = createBrowerRouter([
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./components/Homepage";
+
+const router = createBrowserRouter([
     {
         path: "/",
         element: <Homepage />
-        errorElement: <ErrorPage />
-    },
-    {
-        path: "/",
-        element: 
     }
-])
-
-const Homepage = () => {
-    return (
-        <div>
-            <p>Bonjou!</p>
-        </div>
-    )
-};
+]);
 
 ReactDOM.render(<RouterProvider router={router} />, document.getElementById("app"));
