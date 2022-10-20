01b05573b18b566f8ced85085b9ad37b828808f8
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-20 16:20:02 -0500

    Updated Navbar

--- src/components/Utilites/Navbar.js
+++ src/components/Utilites/Navbar.js
@@ -5,9 +5,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav>
+            <Link to="homepage">Home </Link>
             <Link to="aboutus">About </Link>
             <Link to="login">Log In </Link>
             <Link to="listings">Listings </Link>
+            <Link to="newlisting">New Listing </Link>
+            <Link to="profile">My Profile </Link>
+            <Link to="register">Register </Link>
             <Link to="settings">Settings </Link>
 
         </nav>
